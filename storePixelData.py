34c1b0d5fdfc8c3d3f5de0900d68f90757516e0e
unknown <jeremyvenne@yahoo.com>
2019-11-13 19:04:10 -0500

    Cleaned up code, added comments

--- storePixelData.py
+++ storePixelData.py
@@ -1,21 +1,17 @@
-#import imageio
-#import matplotlib.pyplot as plt
-#from PIL import Image
 import cv2
 import csv
 
-#pic = imageio.imread('greenPic.png')
-#print(pic.shape)
-#print(pic.size)
-#print(pic.max())
-#print(pic.min())
-#print(pic[1,1,1])
+# Current colors being added to data
 colors = ['red', 'orange', 'yellow', 
     'green', 'blue', 'purple','black',
     'white', 'brown']
+# Dark and light colors to improve classification
+darkColors = ['red']
+lightColors = []
 
 csvData = [['Color', 'B', 'G', 'R']]
 
+# Add base colors to data
 for x in colors:
     imageName = 'trainingColors\\' + x + '.png'
     im = cv2.imread(imageName)
@@ -24,6 +20,25 @@ for x in colors:
         pixelSet.append(y)
     csvData.append(pixelSet)
 
+# Add dark colors to data
+for x in darkColors:
+    imageName = 'trainingColors\\dark' + x + '.png'
+    im = cv2.imread(imageName)
+    pixelSet = [x]
+    for y in im[1,1]:
+        pixelSet.append(y)
+    csvData.append(pixelSet)
+
+# Add light colors to data
+for x in lightColors:
+    imageName = 'trainingColors\\light' + x + '.png'
+    im = cv2.imread(imageName)
+    pixelSet = [x]
+    for y in im[1,1]:
+        pixelSet.append(y)
+    csvData.append(pixelSet)
+
+# Write training data to CSV
 with open('data\\pixel.csv', 'w') as csvFile:
     writer = csv.writer(csvFile)
     writer.writerows(csvData)
